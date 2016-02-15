e00ea3d24f7ccaf46b92ce93a6f4305a62c3b7ad
Josh Simpson <k1209590@kcl.ac.uk>
2016-02-15 01:05:22 +0000

    Completed State challenge

--- views/index.jsx
+++ views/index.jsx
@@ -29,9 +29,25 @@
     }
 
     class Todo extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {checked: false};
+        }
+
+        handleChange(e) {
+            this.setState({checked: e.target.checked});
+        }
+        
+        getInitialState(){
+            return {checked: false};
+        }
+        
         render() {
             return (
                 <tr>
+                    <td style={{border: "1px solid black"}}>
+                        <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                    </td>
                     <td style={{border: "1px solid black"}}>{this.props.title}</td>
                     <td style={{border: "1px solid black"}}>{this.props.children}</td>
                 </tr>
@@ -39,7 +55,7 @@
         }
     }
     Todo.propTypes = {
-        title: React.PropTypes.string
+        title: React.PropTypes.string.isRequired
     };
 
     class TodoForm extends React.Component {
